f4377ca0e59633d9c3accf3afc785104427911b9
Karl Horky <karl.horky@gmail.com>
2017-02-14 17:46:46 +0100

    Add failing example

---
+++ routes.js
@@ -0,0 +1,17 @@
+module.exports = {
+	middleware: [
+		'./middleware/request-to-port',
+	],
+	routes: {
+		Index: {
+			path: '/',
+			method: 'get',
+			page: './pages/index',
+		},
+		Network: {
+			path: ['/network'],
+			method: 'get',
+			page: './pages/network',
+		},
+	},
+};
