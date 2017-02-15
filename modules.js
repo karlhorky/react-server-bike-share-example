13b294fe74ec5c3e2e730169257bc9404de2dc45
Karl Horky <karl.horky@gmail.com>
2017-02-15 15:10:37 +0100

    Add module setup

---
+++ modules.js
@@ -0,0 +1,3 @@
+module.exports = {
+  module1: require('@karlhorky/react-server-bike-share-example-module'),
+};
--- routes.js
+++ routes.js
@@ -2,5 +2,5 @@ module.exports = {
 	middleware: [
 		'./middleware/request-to-port',
 	],
-	routes: require('react-server-bike-share-example-module').routes,
+	routes: require('./modules').module1.routes,
 };
